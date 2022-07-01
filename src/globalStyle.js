559a2f6e14ff4b328b3b07187a8bdac3e5871983
dominic.butler <dominic.butler@caristo.com>
2022-07-01 15:47:42 +0100

    installed styled-components

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
 body {
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans",
@@ -9,3 +12,6 @@ body {
 code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
 }
+`;
+
+export default GlobalStyle;
