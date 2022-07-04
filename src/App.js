a92461099dec6414c944ae4d17e5627a1cf718cd
dominic.butler <dominic.butler@caristo.com>
2022-07-04 16:23:36 +0100

    created menu system

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import MainDial from "./MainDial";
 import styled from "styled-components";
-import Swatches from "./Swatches";
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { theme, swatches } from "./theme";
+import Menu from "./Menu";
 
 const Container = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
-    width: 100vw;
+`;
+
+const MenuConnector = styled.div`
+    border-bottom: 1px solid white;
+    width: 50px;
+    margin: 0 20px;
 `;
 
 export default function App() {
@@ -18,8 +27,20 @@ export default function App() {
     return (
         <ThemeProvider theme={theme(clockColor)}>
             <Container>
+                <Menu
+                    activeItem={"colour"}
+                    menu={[
+                        { name: "style" },
+                        { name: "colour" },
+                        { name: "left dial" },
+                        { name: "right dial" },
+                        { name: "bottom dial" }
+                    ]}
+                />
+                <MenuConnector />
                 <MainDial />
-                <Swatches setClockColor={(c) => setClockColor(c)} clockColor={clockColor} />
+                <MenuConnector />
+                <Menu menu={swatches} noMenuKeys onClick={(c) => setClockColor(c)} reverse activeItem={clockColor} />
             </Container>
         </ThemeProvider>
     );
---
+++ src/Menu.js
@@ -0,0 +1,71 @@
+import { transparentize } from "polished";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    position: relative;
+    height: 100%;
+    width: 200px;
+`;
+
+const ContainerInner = styled.div`
+    width: 100%;
+    position: absolute;
+    top: calc(50% - 20px);
+    transform: translateY(${(props) => props.menuOffset}px);
+    transition: all 0.7s cubic-bezier(0, 0, 0, 1.01);
+`;
+
+const MenuItem = styled.div`
+    height: 40px;
+    display: flex;
+    justify-content: ${(props) => (props.reverse ? "flex-start" : "flex-end")};
+    align-items: center;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 18px;
+    cursor: pointer;
+    & .inner-text {
+        transition: 0.2s;
+        padding: 0 4px;
+        background-color: ${(props) => (props.active ? "white" : "transparent")};
+        color: ${(props) => (props.active ? "black" : "white")};
+        border-radius: 4px;
+        display: inline-block;
+    }
+    &:hover .inner-text {
+        background-color: ${(props) => (props.active ? "white" : transparentize(0.7, "white"))};
+    }
+`;
+
+export default function Menu({ menu, noMenuKeys, reverse, onClick, activeItem }) {
+    const [menuOffset, setMenuOffset] = useState(0);
+
+    useEffect(() => {
+        const i = menu.findIndex((m) => {
+            return activeItem === (noMenuKeys ? m : m.name);
+        });
+
+        setMenuOffset(i * -40);
+    }, []);
+
+    return (
+        <Container>
+            <ContainerInner menuOffset={menuOffset}>
+                {menu.map((m, i) => (
+                    <MenuItem
+                        key={i}
+                        onClick={() => {
+                            onClick(noMenuKeys ? m : m.name);
+                            setMenuOffset(i * -40);
+                        }}
+                        active={activeItem === (noMenuKeys ? m : m.name)}
+                        reverse={reverse}
+                    >
+                        <div className="inner-text">{noMenuKeys ? m : m.name}</div>
+                    </MenuItem>
+                ))}
+            </ContainerInner>
+        </Container>
+    );
+}
